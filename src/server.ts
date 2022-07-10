ec2db767dac7e8e121e91f1107e56d4077eddeb9
Dario Baldi <baldi27@gmail.com>
2022-07-10 12:50:24 +0200

    Modify server file to improve port selection

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
 
 const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+const port: number= 3333
 
 app.use(bodyParser.json())
 
@@ -10,6 +10,6 @@ app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
+app.listen(port, function () {
+    console.log(`starting app on port: ${port}`)
 })
