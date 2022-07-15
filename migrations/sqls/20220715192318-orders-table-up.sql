3fc0cca3eac04a19691546399cc82a3eff59a293
Dario Baldi <baldi27@gmail.com>
2022-07-15 21:39:19 +0200

    Add foreign key to orders table

--- migrations/sqls/20220715192318-orders-table-up.sql
+++ migrations/sqls/20220715192318-orders-table-up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
-    status VARCHAR(255) NOT NULL
+    status VARCHAR(255) NOT NULL,
+    CONSTRAINT user_ref FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL
 );
