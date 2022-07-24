efbdbaaa5bae2e20df4dc7d755b6e4060544f0e1
Dario Baldi <baldi27@gmail.com>
2022-07-24 09:09:55 +0200

    Add username to user table

--- migrations/sqls/20220714202357-user-table-up.sql
+++ migrations/sqls/20220714202357-user-table-up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     password_hash VARCHAR(255) NOT NULL
